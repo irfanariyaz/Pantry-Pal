0ef0974436497b37bb259dd05fdcd7d06c923ffe
Your Name <irfana.27@gmail.com>
2024-04-25 00:41:25 -0700

    updated port

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       path: "/",
-      httpOnly: false,
+      httpOnly: true,
       secure: false,
       maxAge: 1000 * 60 * 60 * 24,
     }, 
